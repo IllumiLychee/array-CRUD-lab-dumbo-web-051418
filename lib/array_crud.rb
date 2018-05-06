e64736f4f44fdd7dacdecb71fd1764cff8ab6016
Angelina Wong <angelina.w25@gmail.com>
2018-05-06 22:10:29 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,8 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  fruit_array = Array.new
-  fruit_array = [apple, orange, mango, watermelon]
+  fruit_array = ['apple', 'orange', 'mango', 'watermelon']
 end
 
 def add_element_to_end_of_array(array, element)
